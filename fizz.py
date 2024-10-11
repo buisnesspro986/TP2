506394ad9ecb6a2128477b3a1a676396bc17c769
buisnesspro986 <buisnesspromail@gmail.com>
2024-10-11 12:24:51 +0200

    code fini et fonctionnel

--- fizz.py
+++ fizz.py
@@ -1,5 +1,14 @@
-a=1
-b=2
-c=a+b
-print(c)
+def affiche():
+    result = []
+    for i in range(1, 101):
+        if i % 3 == 0 and i % 5 == 0:
+            result.append("FrisBee")
+        elif i % 3 == 0:
+            result.append("Fizz")
+        elif i % 5 == 0:
+            result.append("Buzz")
+        else:
+            result.append(str(i))
+    print("".join(result))
+affiche()
 
