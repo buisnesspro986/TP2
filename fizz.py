87aea722c7368885fb319e2ef72ab7c17c05b523
buisnesspro986 <buisnesspromail@gmail.com>
2024-10-11 13:03:42 +0200

    fichier test realiser mais erreur sur ligne 2 test

--- fizz.py
+++ fizz.py
@@ -1,17 +1,22 @@
-def affiche(n1, n2):
-    result = []
-    for i in range(n1, n2 + 1):
-        if i % 3 == 0 and i % 5 == 0:
-            result.append("FrisBee")
-        elif i % 3 == 0:
-            result.append("Fizz")
-        elif i % 5 == 0:
-            result.append("Buzz")
-        else:
-            result.append(str(i))
-    print("".join(result))
-affiche(5, 10)
-affiche(10, 16)
+class FizzBuzz:
+    def affiche(self, n1, n2):
+        result = []
+        for i in range(n1, n2 + 1):
+            if i % 3 == 0 and i % 5 == 0:
+                result.append("FrisBee")
+            elif i % 3 == 0:
+                result.append("Fizz")
+            elif i % 5 == 0:
+                result.append("Buzz")
+            else:
+                result.append(str(i))
+        return "".join(result)
+
+
+fizzbuzz = FizzBuzz()
+print(fizzbuzz.affiche(5, 10))  
+print(fizzbuzz.affiche(10, 16)) 
+
 
 
 
