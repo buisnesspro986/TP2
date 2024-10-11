5fe1a4ae12855ff44e21dd2e9834e943791ebda5
buisnesspro986 <buisnesspromail@gmail.com>
2024-10-11 12:29:03 +0200

    premiere evolution de code effectuer tu peux passer a le deuxieme

--- fizz.py
+++ fizz.py
@@ -1,14 +1,15 @@
-def affiche():
+def affiche(n):
     result = []
-    for i in range(1, 101):
+    for i in range(1, n + 1):
         if i % 3 == 0 and i % 5 == 0:
             result.append("FrisBee")
         elif i % 3 == 0:
             result.append("Fizz")
-        elif == 0:
+        elif i % 5 == 0:
             result.append("Buzz")
         else:
             result.append(str(i))
     print("".join(result))
-affiche()
+affiche(15)
+
 
